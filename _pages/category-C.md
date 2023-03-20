3a3a257997ff9e8e51ca863b9fa5fc973427004d
dudxor4587 <68459414+dudxor4587@users.noreply.github.com>
2023-03-21 03:41:21 +0900

    replace repository

---
+++ _pages/category-C.md
@@ -0,0 +1,12 @@
+---
+title: "C"
+layout: archive
+permalink: /C
+author_profile: true
+sidebar:
+    nav: "sidebar-category"
+---
+
+
+{% assign posts = site.categories.C %}
+{% for post in posts %} {% include archive-single.html type=page.entries_layout %} {% endfor %}
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "[2023-03-09]CP03 3주차 실습",
+        "excerpt":"팩토리얼 수 구하기 이 문제는 팩토리얼의 수를 구하는 문제입니다. Input Format 한 개의 정수 n을 입력합니다. 이 때, n은 정수의 범위를 초과하지 않습니다. Output Format 출력 형식은 Sample Output과 같은 형식을 따릅니다. 한 줄에 팩토리얼(factorial)수를 출력합니다. 출력이 끝나면 개행문자를 출력합니다. Sample Input 0 5 Sample Output 0 120 풀이과정 int...","categories": ["C"],
+        "tags": [],
+        "url": "/c/CP03_03/",
+        "teaser": null
+      }]
